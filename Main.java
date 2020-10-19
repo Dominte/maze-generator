6d101ccd5fc28d6a2090f89cb917fb813f481384
Dominte <59875175+Dominte@users.noreply.github.com>
2020-10-19 19:15:38 +0300

    fdsafdsafsdfas

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ public class Main {
         System.out.println(mazeSolution.getSymbolicMaze());
 
         mazeSolution.getSpecialStates();
-        System.out.println(mazeSolution.getRawMaze());
-        System.out.println(mazeSolution.isSolved());
+        mazeSolution.backTrackingMaze();
+
     }
 }
--- MazeSolution.java
+++ MazeSolution.java
@@ -5,6 +5,7 @@ public class MazeSolution {
     int dimension;
     Node finish;
     Node start;
+    int pas;
 
     MazeSolution(int[][] maze) {
         this.maze = maze;
@@ -22,8 +23,8 @@ public class MazeSolution {
     MazeSolution(int[][] maze, int xStart, int xFinish, int yStart, int yFinish) {
         this.maze = maze;
         dimension = maze.length;
-        maze[xStart][xFinish]=1;
-        maze[yStart][yFinish]=-2;
+        maze[xStart][xFinish] = 1;
+        maze[yStart][yFinish] = -2;
         start = new Node(xStart, xFinish);
         finish = new Node(yStart, yFinish);
     }
@@ -39,7 +40,7 @@ public class MazeSolution {
         }
     }
 
-    public boolean isSolved() {
+    public boolean isSolved() { //????????????????
 
         if (maze[finish.x - 1][finish.y] != -1 && maze[finish.x - 1][finish.y] != 0)
             return true;
@@ -81,4 +82,68 @@ public class MazeSolution {
         }
         return sb.toString();
     }
+
+    public String getSymbolicMaze(int[][] sol) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < dimension; i++) {
+            for (int j = 0; j < dimension; j++) {
+
+                if (sol[i][j] == -1)
+                    sb.append("*");
+                else if (sol[i][j] == -2)
+                    sb.append("F");
+                else if (sol[i][j] == 1)
+                    sb.append("S");
+                else
+                    sb.append(" ");
+
+                sb.append("  ");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+    public boolean isSafe(int x, int y) {
+        if (x >= 0 && x < dimension && y >= 0 && y < dimension && maze[x][y] == 0)
+            return true;
+        if (x >= 0 && x < dimension && y >= 0 && y < dimension && maze[x][y] == -2)
+            return true;
+        return false;
+    }
+
+    public void backTrackingMaze() {
+
+
+        if (!solveMaze(start.x, start.y)) {
+            System.out.println("Solution doesn't exist");
+
+        } else System.out.println(getSymbolicMaze());
+    }
+
+    public boolean solveMaze(int x, int y) {
+        if (x == finish.x && y == finish.y && maze[x][y] == -2) {
+            return true;
+        }
+
+        if (isSafe(x, y)) {
+            maze[x][y] =1;
+
+            if (solveMaze(x + 1, y ))
+                return true;
+            if (solveMaze(x, y + 1))
+                return true;
+            if (solveMaze(x, y - 1))
+                return true;
+            if (solveMaze(x - 1, y))
+                return true;
+
+            maze[x][y]=0;
+            return false;
+        }
+
+        return false;
+    }
+
+
 }
